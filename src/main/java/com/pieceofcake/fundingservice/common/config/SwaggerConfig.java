0e32fe996f50060d393d348701b749759cfedd10
Sujin31 <52381101+Sujin31@users.noreply.github.com>
2025-06-23 09:48:39 +0900

    Update SwaggerConfig.java

--- src/main/java/com/pieceofcake/fundingservice/common/config/SwaggerConfig.java
+++ src/main/java/com/pieceofcake/fundingservice/common/config/SwaggerConfig.java
@@ -29,7 +29,7 @@ public class SwaggerConfig {
         return new OpenAPI()
                 .addSecurityItem(securityRequirement)
                 .components(components)
-                .addServersItem(new Server().url("/piece-service"))
+                .addServersItem(new Server().url("/funding-service"))
                 .info(apiInfo());
     }
 
