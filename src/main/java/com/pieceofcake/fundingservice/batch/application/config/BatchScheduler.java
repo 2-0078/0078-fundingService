3ab6b0cb1ef86ee7b7f99ca3196934ad965d2bfe
sujin31 <dltnwls301@gmail.com>
2025-06-30 15:49:27 +0900

    [refactor] 수정사항 - 공모 마감 시 상태 변경 이벤트 없애기 - (임시) 배치 스케줄 변경

--- src/main/java/com/pieceofcake/fundingservice/batch/application/config/BatchScheduler.java
+++ src/main/java/com/pieceofcake/fundingservice/batch/application/config/BatchScheduler.java
@@ -17,7 +17,8 @@ public class BatchScheduler {
     private final JobLauncher jobLauncher;
     private final Job dailyFundingJob;
 
-    @Scheduled(cron = "0 0 14 * * *")
+    //임시 시간 설정
+    @Scheduled(cron = "0 0/30 * * * *")
     public void runDailyFundingJob() throws Exception {
         JobParameters jobParameters = new JobParametersBuilder()
                 .addString("time", LocalDateTime.now().toString())
